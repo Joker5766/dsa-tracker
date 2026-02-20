552e7ceebec2037a06649b19ab87728e783db13c
Joker <pranavchavan5766@gmail.com>
2026-02-20 22:08:26 +0530

    feat: setup routing and basic group dashboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import GroupDashboard from "./pages/GroupDashboard";
 
 function App() {
-  return <Landing />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/group" element={<GroupDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
